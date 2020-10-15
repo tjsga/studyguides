6105a077b75c687f7e93de0322e5249bf87e7c2a
Akash Bhave <akashbbhave@gmail.com>
2020-10-15 15:42:10 -0400

    Link to course on home page

--- studyguides/apps/courses/models.py
+++ studyguides/apps/courses/models.py
@@ -7,7 +7,7 @@ from django.core.validators import RegexValidator, FileExtensionValidator
 class Subject(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
-    url = models.CharField(max_length=20, unique=True, validators=[RegexValidator(
+    url = models.CharField("URL", max_length=20, unique=True, validators=[RegexValidator(
         regex="^[a-zA-Z0-9_\-]+$", message="Only alphanumeric, dashes, and underscores allowed")])
 
     courses = models.ManyToManyField("Course", related_name="subject")
@@ -19,6 +19,8 @@ class Subject(models.Model):
 class Course(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100, unique=True)
+    url = models.CharField("URL", max_length=20, unique=True, validators=[RegexValidator(
+        regex="^[a-zA-Z0-9_\-]+$", message="Only alphanumeric, dashes, and underscores allowed")])
 
     def __str__(self):
         return self.name
