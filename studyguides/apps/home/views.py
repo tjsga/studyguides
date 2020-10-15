74bacca1e3c65595344ab645e02c07e2712e9972
Akash Bhave <akashbbhave@gmail.com>
2020-10-15 15:24:18 -0400

    Design basic home page

--- studyguides/apps/home/views.py
+++ studyguides/apps/home/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.apps import apps
 
-# Create your views here.
+Subject = apps.get_model("courses", "Subject")
+Course = apps.get_model("courses", "Course")
+
 
 def index_view(request):
-   return render(request, "base.html")
-   #return HttpResponse("hello world")
-
-
+    return render(request, "home.html", {"subjects": [(subject, Course.objects.filter(subject=subject)) for subject in Subject.objects.all()]})
