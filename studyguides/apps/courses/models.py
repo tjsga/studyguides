6aab8f61c4540968e799a4005e76ef9befca1a13
Invinceaman <invinceaman023@gmail.com>
2020-10-20 17:38:51 -0400

    add tags to guides

--- studyguides/apps/courses/models.py
+++ studyguides/apps/courses/models.py
@@ -8,7 +8,7 @@ class Subject(models.Model):
     url = models.CharField("URL", max_length=20, unique=True, validators=[RegexValidator(
         regex="^[a-zA-Z0-9_\-]+$", message="Only alphanumeric, dashes, and underscores allowed")])
 
-    courses = models.ManyToManyField("Course", related_name="subject")
+    courses = models.ManyToManyField("Course", related_name="subject", null=True, blank=True)
 
     def __str__(self):
         return self.name
@@ -27,10 +27,15 @@ class Course(models.Model):
 class Guide(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
-    teacher = models.CharField(max_length=100, blank=True)
 
+    tags = models.ManyToManyField("Tag", null=True)
     course = models.ForeignKey(Course, on_delete=models.CASCADE, null=True)
     url = models.URLField(max_length=300)
 
     def __str__(self):
         return f"[{self.course or 'N/A'}{(', ' + self.teacher) if self.teacher else ''}] {self.name}"
+
+class Tag(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.SlugField(max_length=100, unique=True, validators=[RegexValidator(
+        regex="^[a-z0-9_\-]+$", message="Only lowercase alphanumeric, dashes, and underscores allowed")])
