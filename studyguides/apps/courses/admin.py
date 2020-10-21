85d9c02fed6c45ab9b14b38a76b62d6f633add63
lauren <pmd7211@gmail.com>
2020-10-21 17:54:25 -0400

    register tag model

--- studyguides/apps/courses/admin.py
+++ studyguides/apps/courses/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Subject, Course, Guide
+from .models import Subject, Course, Guide, Tag
 # Register your models here.
 
 admin.site.register(Subject)
 admin.site.register(Course)
 admin.site.register(Guide)
+admin.site.register(Tag)
